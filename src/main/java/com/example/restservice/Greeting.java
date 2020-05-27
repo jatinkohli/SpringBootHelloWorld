ff4d5c899be3ef4ccb0a8d0a104be41d7c7f9bed
Jatin Kohli <jatinkohli>
2020-05-26 19:27:41 -0700

    POST Greeting works

--- src/main/java/com/example/restservice/Greeting.java
+++ src/main/java/com/example/restservice/Greeting.java
@@ -2,19 +2,11 @@ package com.example.restservice;
 
 public class Greeting {
 
-	private final long id;
-	private final String content;
+	public final long id;
+	public final String content;
 
 	public Greeting(long id, String content) {
 		this.id = id;
 		this.content = content;
-	}
-
-	public long getId() {
-		return id;
-	}
-
-	public String getContent() {
-		return content;
 	}
 }
--- src/main/java/com/example/restservice/GreetingController.java
+++ src/main/java/com/example/restservice/GreetingController.java
@@ -3,17 +3,23 @@ package com.example.restservice;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetingController {
 
-	private static final String template = "Hello, %s!";
-	private final AtomicLong counter = new AtomicLong();
+	private final AtomicLong counterWQ = new AtomicLong(1);
+	private final AtomicLong counterNQ = new AtomicLong(1);
 
-	@GetMapping("/greeting")
-	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	@GetMapping("/greetingWQ")
+	public Greeting greetingWithQuery(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return new Greeting(counterWQ.getAndIncrement(), "Hello, " + name + "!");
+	}
+
+	@PostMapping("/greetingNQ")
+	public Greeting greetingWithoutQuery(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return new Greeting(counterNQ.getAndIncrement(), "Hello, " + name + "!");
 	}
 }
