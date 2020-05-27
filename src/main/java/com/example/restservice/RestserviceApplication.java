5ea57b501fa9561e09d66237d2a0b1fb92c3432c
Jatin Kohli <jatinkohli>
2020-05-26 19:29:52 -0700

    Documentation

--- src/main/java/com/example/restservice/RestserviceApplication.java
+++ src/main/java/com/example/restservice/RestserviceApplication.java
@@ -3,6 +3,10 @@ package com.example.restservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Start with "mvnw spring-boot:run" in the top level directory.
+ * Server started at http://localhost:8080/ with endpoints "greetingWQ" and "greeting NQ"
+ */
 @SpringBootApplication(scanBasePackages = {"com"})
 public class RestserviceApplication {
 
